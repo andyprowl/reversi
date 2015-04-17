a90dd4cd5b2d5e215808a780475b92dee04f2c9c
andrea.proli <andrea.proli@fei.com>
2015-04-17 20:58:56 +0200

    Temporarily remove incomplete projects for multi-player support.

--- game_server_logic_unit_tests/include/stdafx.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "testing/naming.hpp"
-
-#include <boost/optional/optional_io.hpp>
-
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
--- game_server_logic_unit_tests/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.hpp"
-
-int main(int argc, char** const argv) 
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    
-    return RUN_ALL_TESTS();
-}
